84ae66084337f5d8880bceeececf0a4e117de2bd
JeffreySu <www.jeffrey.su@gmail.com>
2020-02-07 23:28:45 +0800

    添加 XscfModule 数据库对象

---
+++ src/Basic/Senparc.Scf.Core/Models/DataBaseModel/Dto/XscfModuleDto.cs
@@ -0,0 +1,35 @@
+﻿using Senparc.Core.Models.DataBaseModel;
+using Senparc.Scf.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Senparc.Scf.Core.Models.DataBaseModel
+{
+    public class XscfModuleDto:DtoBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Uid { get; set; }
+        public string MenuName { get; set; }
+        public bool AllowRemove { get; set; }
+        public XscfModules_State State { get; set; }
+    }
+
+    public class CreateOrUpdate_XscfModuleDto : DtoBase
+    {
+        [Required,StringLength(100)]
+        public string Name { get; set; }
+        [Required, StringLength(100)]
+        public string Uid { get; set; }
+        [Required, StringLength(100)]
+        public string MenuName { get; set; }
+        [Required]
+        public bool AllowRemove { get; set; }
+        [Required]
+        public XscfModules_State State { get; set; }
+
+    }
+
+}
---
+++ src/Basic/Senparc.Scf.Core/Models/DataBaseModel/Mapping/ConfigurationMappingWithIdBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.Core.Models.DataBaseModel
+{
+    /// <summary>
+    /// 包含 Id（Key）的 ConfigurationMapping 基类
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public class ConfigurationMappingWithIdBase<TEntity,TKey> : IEntityTypeConfiguration<TEntity>
+        where TEntity : EntityBase<TKey>
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(z => z.Id);
+            builder.Property(e => e.AddTime).HasColumnType("datetime").IsRequired();
+            builder.Property(e => e.LastUpdateTime).HasColumnType("datetime").IsRequired();
+        }
+    }
+}
