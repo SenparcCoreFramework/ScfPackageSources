df658a1943aa30d2e43ac1379e0d5080337babb8
JeffreySu <www.jeffrey.su@gmail.com>
2020-02-16 17:40:39 +0800

    modify

--- src/Extensions/Senparc.Xscf.ChangeNamespace/Functions/ChangeNameSpace.cs
+++ src/Extensions/Senparc.Xscf.ChangeNamespace/Functions/ChangeNameSpace.cs
@@ -14,7 +14,7 @@ namespace Senparc.Xscf.ChangeNamespace.Functions
                 new FunctionParam("新命名空间","命名空间根，必须以.结尾，用于替换[Senparc.Scf.]", TypeCode.String)
                 };
 
-        public ChangeNamespace(IServiceProvider serviceProvider):base(serviceProvider)
+        public ChangeNamespace(IServiceProvider serviceProvider) : base(serviceProvider)
         {
         }
 
@@ -43,7 +43,7 @@ namespace Senparc.Xscf.ChangeNamespace.Functions
 
             foreach (var item in meetRules)
             {
-                var files = Directory.GetFiles(path, item.FileType,SearchOption.AllDirectories);
+                var files = Directory.GetFiles(path, item.FileType, SearchOption.AllDirectories);
                 base.RecordLog(sb, $"文件类型:{item.FileType} 数量:{files.Length}");
 
                 foreach (var file in files)
@@ -68,8 +68,8 @@ namespace Senparc.Xscf.ChangeNamespace.Functions
                             using (var sw = new StreamWriter(fs))
                             {
                                 sw.Write(content);
+                                sw.Flush();
                             }
-                            fs.Flush();
                             fs.Close();
                         }
                     }
