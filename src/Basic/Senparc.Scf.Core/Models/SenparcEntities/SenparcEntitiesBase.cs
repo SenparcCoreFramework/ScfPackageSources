48c1280c50352575e5bf171d4bd500a6942ce4a6
苏震巍 <zsu@senparc.com>
2020-02-25 20:26:04 +0800

    添加 SenparcEntitiesBase

---
+++ src/Basic/Senparc.Scf.Core/Models/SenparcEntities/SenparcEntitiesBase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Senparc.Scf.Core.Models.SenparcEntities
+{
+    /// <summary>
+    /// SenparcEntities 的基类
+    /// </summary>
+    public abstract class SenparcEntitiesBase : DbContext, ISenparcEntities
+    {
+        private static readonly bool[] _migrated = { true };
+
+        public SenparcEntitiesBase([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// 执行 EF Core 的合并操作（等价于 update-database）
+        /// <para>出于安全考虑，每次执行 Migrate() 方法之前，必须先执行 ResetMigrate() 开启允许 Migrate 执行的状态。</para>
+        /// </summary>
+        public virtual void Migrate()
+        {
+            if (!_migrated[0])
+            {
+                lock (_migrated)
+                {
+                    if (!_migrated[0])
+                    {
+                        Database.Migrate(); // apply all migrations
+                        _migrated[0] = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 重置合并状态
+        /// </summary>
+        public virtual void ResetMigrate()
+        {
+            _migrated[0] = false;
+        }
+
+        /// <summary>
+        /// 全局查询，附带软删除状态
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder"></param>
+        public virtual void SetGlobalQuery<T>(ModelBuilder builder) where T : EntityBase
+        {
+            builder.Entity<T>().HasQueryFilter(z => !z.Flag);
+        }
+    }
+}
