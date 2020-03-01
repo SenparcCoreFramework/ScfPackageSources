bd2ba7349224f7fa7d59ac5e7310a02115199a9a
zhao365845726 <MartyZane@hotmail.com>
2020-03-01 15:35:18 +0800

    增加终端命令操作

---
+++ src/Extensions/Senparc.Xscf.Terminal/Functions/Terminal.cs
@@ -0,0 +1,109 @@
+﻿using Senparc.CO2NET.Extensions;
+using Senparc.Scf.XscfBase;
+using Senparc.Scf.XscfBase.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Senparc.Xscf.Terminal.Functions
+{
+    public class Terminal_Parameters : IFunctionParameter
+    {
+        [MaxLength(300)]
+        [Description("命令行，如：dir /?")]
+        public string CommandLine { get; set; }
+    }
+
+    public class Terminal : FunctionBase
+    {
+        //注意：Name 必须在单个 Xscf 模块中唯一！
+        public override string Name => "命令提示符";
+
+        public override string Description => "输入Windows命令提示符中的命令,即可返回相应的结果";
+
+        public override Type FunctionParameterType => typeof(Terminal_Parameters);
+
+        public Terminal(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        /// <summary>
+        /// 运行
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public override FunctionResult Run(IFunctionParameter param)
+        {
+            var typeParam = param as Terminal_Parameters;
+
+            FunctionResult result = new FunctionResult()
+            {
+                Success = true
+            };
+
+            StringBuilder sb = new StringBuilder();
+            base.RecordLog(sb, "开始运行 Terminal");
+            string strExecRes = string.Empty;
+            if (!string.IsNullOrEmpty(typeParam.CommandLine))
+            {
+                strExecRes = ExeCommand($"{typeParam.CommandLine}");
+            }
+            else
+            {
+                strExecRes = ExeCommand($"dir");
+            }
+            sb.AppendLine(strExecRes);
+            result.Log = sb.ToString();
+            result.Message = "操作成功！";
+            return result;
+        }
+
+        /// <summary>
+        /// 执行cmd.exe命令
+        /// </summary>
+        /// <param name="commandText">命令文本</param>
+        /// <returns>命令输出文本</returns>
+        private string ExeCommand(string commandText)
+        {
+            return ExeCommand(new string[] { commandText });
+        }
+        /// <summary>
+        /// 执行多条cmd.exe命令
+        /// </summary>
+        /// <param name="commandTexts">命令文本数组</param>
+        /// <returns>命令输出文本</returns>
+        private string ExeCommand(string[] commandTexts)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            string strOutput = null;
+            try
+            {
+                p.Start();
+                foreach (string item in commandTexts)
+                {
+                    p.StandardInput.WriteLine(item);
+                }
+                p.StandardInput.WriteLine("exit");
+                strOutput = p.StandardOutput.ReadToEnd();
+                //strOutput = Encoding.UTF8.GetString(Encoding.Default.GetBytes(strOutput));
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+                strOutput = e.Message;
+            }
+            return strOutput;
+        }
+    }
+}
---
+++ src/Extensions/Senparc.Xscf.Terminal/Register.cs
@@ -0,0 +1,45 @@
+﻿using Senparc.Scf.Core.Enums;
+using Senparc.Scf.XscfBase;
+using Senparc.Xscf.Terminal.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senparc.Xscf.Terminal
+{
+    [XscfRegister]
+    public class Register : IXscfRegister
+    {
+        public Register()
+        { }
+
+        #region IRegister 接口
+
+        public string Name => "Senparc.Xscf.Terminal";
+        public string Uid => "600C608A-F99A-4B1B-A18E-8CE69BE8DA92";//必须确保全局唯一，生成后必须固定
+        public string Version => "0.0.5";//必须填写版本号
+
+        public string MenuName => "终端模块";
+        public string Description => "此模块提供给开发者一个可以直接使用终端命令控制系统的模块！";
+
+        /// <summary>
+        /// 注册当前模块需要支持的功能模块
+        /// </summary>
+        public IList<Type> Functions => new[] { 
+            typeof(Functions.Terminal),
+        };
+
+        public virtual Task InstallOrUpdateAsync(InstallOrUpdate installOrUpdate)
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task UninstallAsync(Func<Task> unsinstallFunc)
+        {
+            await unsinstallFunc().ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
