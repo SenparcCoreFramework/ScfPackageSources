db203dd57aadf920f002d33f57c29ab35db414e4
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-03 13:46:05 +0800

    抽象集成 Senparc.Scf.AreaBase

--- src/Basic/Senparc.Scf.Core/Models/DataBaseModel/Dto/XscfModuleDto.cs
+++ src/Basic/Senparc.Scf.Core/Models/DataBaseModel/Dto/XscfModuleDto.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Senparc.Core.Models.DataBaseModel;
 using Senparc.Scf.Core.Enums;
 using System;
 using System.Collections.Generic;
---
+++ src/Basic/Senparc.Scf.Core/WorkContext/Provider/IAdminWorkContextProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Senparc.Scf.Core.WorkContext.Provider
+{
+    public interface IAdminWorkContextProvider
+    {
+        AdminWorkContext GetAdminWorkContext();
+    }
+}
