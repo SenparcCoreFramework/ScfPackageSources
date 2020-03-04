4f2f7f23fe63423804e4bfd1236102b5f5522e8e
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-04 18:15:32 +0800

    XscfModuleBase 0.3.105.1-beta1

---
+++ src/Basic/Senparc.Scf.XscfBase/Exceptions/ScfModuleException.cs
@@ -0,0 +1,22 @@
+﻿using Senparc.Scf.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.XscfBase
+{
+    public class XscfPageException : SCFExceptionBase
+    {
+        public IXscfRegister XscfRegister { get; set; }
+
+        public XscfPageException(IXscfRegister xscfRegister, string message, bool logged = false) : base(message, logged)
+        {
+            XscfRegister = xscfRegister;
+        }
+
+        public XscfPageException(IXscfRegister xscfRegister, string message, Exception inner, bool logged = false) : base(message, inner, logged)
+        {
+            XscfRegister = xscfRegister;
+        }
+    }
+}
