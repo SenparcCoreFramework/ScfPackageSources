85f99f3a7b145332679397fe0ce05c6df079c586
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-06 21:52:07 +0800

    重新创建继承Test项目

--- src/Senparc.IntegrationSample.Tests/Senparc.IntegrationSample/Startup.cs
+++ src/Senparc.IntegrationSample.Tests/Senparc.IntegrationSample/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Senparc.Scf.SMS;
 
 namespace Senparc.IntegrationSample
 {
@@ -18,15 +17,13 @@ namespace Senparc.IntegrationSample
         {
             Configuration = configuration;
         }
+
         public IConfiguration Configuration { get; }
-        public IWebHostEnvironment env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-            .Configure<SenparcSmsSetting>(Configuration.GetSection("SenparcSmsSetting"))//TODO£ºÈÃSMSÄ£¿é½øÐÐ×¢²á
-            ;
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
