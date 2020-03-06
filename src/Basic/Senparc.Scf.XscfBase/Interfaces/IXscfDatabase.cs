c1f3ccae14427a2b3d4bd1df2e268e4030927770
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-06 21:50:58 +0800

    XSCF add IXscfDatabase

---
+++ src/Basic/Senparc.Scf.XscfBase/Interfaces/IXscfDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.XscfBase.Interfaces
+{
+    /// <summary>
+    /// XSCF 模块数据库配置
+    /// </summary>
+    public interface IXscfDatabase
+    {
+        /// <summary>
+        /// 全局唯一的前缀，务必避免和其他模块重复
+        /// </summary>
+        string UniquePrefix { get; }
+    }
+}
