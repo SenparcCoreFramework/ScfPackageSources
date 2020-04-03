92e92a7804c7ab741b1f1796a14e43d2477ea5b7
JeffreySu <www.jeffrey.su@gmail.com>
2020-04-03 12:35:31 +0800

    添加 XscfAutoConfigurationMappingAttribute，分配新版本号

---
+++ src/Basic/Senparc.Scf.XscfBase/Attributes/XscfAutoConfigurationMappingAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.XscfBase.Attributes
+{
+    /// <summary>
+    /// 自动配置 ConfigurationMapping 特性
+    /// </summary>
+    public class XscfAutoConfigurationMappingAttribute : Attribute
+    {
+    }
+}
