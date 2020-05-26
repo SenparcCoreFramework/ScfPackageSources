21786247ecf88be70a0b26d048cc4d07a56241a7
JeffreySu <www.jeffrey.su@gmail.com>
2020-05-26 23:16:39 +0800

    完善注释

--- src/Basic/Senparc.Scf.XscfBase/Attributes/XscfOrderAttribute.cs
+++ src/Basic/Senparc.Scf.XscfBase/Attributes/XscfOrderAttribute.cs
@@ -12,14 +12,14 @@ namespace Senparc.Scf.XscfBase
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="order">参考数值：0：普通（默认），1-:5000：需要预加载的重要模块，>5000：系统及基础模块</param>
+        /// <param name="order">加载顺序，数字越大加载顺序越靠前。请严格按照参考数值：0：普通（默认），1-5000：需要预加载的重要模块，>5000：系统及基础模块</param>
         public XscfOrderAttribute(int order)
         {
             Order = order;
         }
 
         /// <summary>
-        /// 参考数值：0：普通（默认），1-:5000：需要预加载的重要模块，>5000：系统及基础模块
+        /// 加载顺序，数字越大加载顺序越靠前。请严格按照参考数值：0：普通（默认），1-5000：需要预加载的重要模块，>5000：系统及基础模块
         /// </summary>
         public int Order { get; set; }
     }
